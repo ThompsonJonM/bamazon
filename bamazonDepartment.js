1be266776be54f44ec71c0ffb4c0561569eeb6c9
unknown <thompson.jonm@gmail.com>
2017-08-04 12:03:46 -0500

    added dept dependencies

---
+++ bamazonDepartment.js
@@ -0,0 +1,20 @@
+// Dependencies
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// MySQL connect
+var connection = mysql.createConnection ({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'bamazondb',
+});
+
+// Connection functions
+connection.connect (function(err) {
+    if (err) throw err;
+
+    console.log("Connected as " + connection.threadId);
+    bamazonDepartment();
+});
